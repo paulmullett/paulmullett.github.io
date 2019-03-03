93bd141f373637c92f1e30d0801de1ecd401fd9e
Paul Mullett <paul@paulmullett.com>
2019-03-03 11:15:02 +0000

    First round of updates

---
+++ js/lte-ie7.js
@@ -0,0 +1,29 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'social\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-flickr' : '&#x21;',
+			'icon-dribbble' : '&#x22;',
+			'icon-linkedin' : '&#x23;',
+			'icon-mail' : '&#x24;',
+			'icon-twitter' : '&#x25;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
